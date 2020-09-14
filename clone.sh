fcf4de515530b672bdfbca438f6e503c9c9848c6
Jonathan Scherrer <jonathan.s.scherrer@gmail.com>
2020-09-14 17:02:33 +0200

    fixes

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 _base=archlinux-base
 _new_name=$1
@@ -7,8 +7,8 @@ source ./utils.sh
 
 virt-clone \
     --original ${_base} \
-    --name ${_new_name} \
-    -f /var/lib/libvirt/images/${_new_name}.qcow2 || exit 1
+    --name "${_new_name}" \
+    -f "/var/lib/libvirt/images/${_new_name}.qcow2" || exit 1
 
 sudo virt-sysprep -d "${_new_name}" \
     --operations bash-history,dhcp-client-state,machine-id,lvm-uuids,logfiles,ssh-hostkeys,customize || exit 1
@@ -20,18 +20,19 @@ until
 do
     sleep 1
 done
-echo "$_ip"
-while ! nc -z "${_ip}" 22 > /dev/null 2>&1; do
+
+until
+    nc -z "${_ip}" 22 > /dev/null 2>&1
+do
     echo "Waiting for ssh"
     sleep 2
 done
 
 ssh-keyscan "${_ip}" >> ~/.ssh/known_hosts
 
-ssh "${_ip}" <<EOS
-sudo hostnamectl set-hostname ${_new_name}.kvm.bbrain.io
-sudo systemctl restart systemd-{networkd,resolved}
-EOS
+ssh "${_ip}" \
+    sudo hostnamectl set-hostname "${_new_name}.kvm.bbrain.io" \
+    sudo systemctl restart systemd-{networkd,resolved}
 
 ssh-keyscan "${_new_name}.kvm.bbrain.io" >> ~/.ssh/known_hosts
 
